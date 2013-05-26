f7a03c9038a827c165b744b01fc0a2d577b60191
Matias Anaya <matiasanaya@gmail.com>
2013-05-26 14:19:21 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialAcademyMockup::Application.config.secret_token = '15879cf6dde47f8cd3fc9cf9c9e36c0be120afde52036ff9d5aa02f45b59d16122314dfb490aa4b122721cc4bed935c3a89a12463f4168065a81a7f2228fa966'
